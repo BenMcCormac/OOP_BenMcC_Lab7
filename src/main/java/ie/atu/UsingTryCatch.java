44a8e4c76ca5e677a36becf05a6f8a81302c4630
BenMcCormack <g00416547@atu.ie>
2023-11-13 09:45:56 +0000

    Put in try Catch

---
+++ src/main/java/ie/atu/UsingTryCatch.java
@@ -0,0 +1,34 @@
+package ie.atu;
+
+import java.util.Scanner;
+
+public class UsingTryCatch
+{
+    public static void main(String[] args) {
+        try
+        {
+            System.out.println("Please enter a number between 0 - 9 :");
+            Scanner myscan = new Scanner(System.in);
+
+            //Read user input as aString
+            String input = myscan.nextLine();
+
+            //Parse the input to an integer
+            int myNum = Integer.parseInt(input);
+
+            //Check if the number is between 0 and 9
+            if (myNum >= 0 && myNum <= 9)
+            {
+                System.out.println("You inputted " + myNum);
+            }
+            else
+            {
+                System.out.println("Not a valid number");
+            }
+        }
+        catch (NumberFormatException e)
+        {
+            System.out.println("Not a valid input");
+        }
+    }
+}
